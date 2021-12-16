c7d80ae8170a848c5e99ccb8a406db12cd19f019
Matthias FERRAINA <mferraina.prestataire@si2m.fr>
2021-12-16 13:45:43 +0100

    first commit

---
+++ main.ts
@@ -0,0 +1,17 @@
+import { Application, config, Router } from "./deps.ts";
+
+try {
+  const conf = config();
+  const router = new Router();
+  router.get("/", (context) => {
+    context.response.body = conf.MESSAGE;
+  });
+
+  const app = new Application();
+  app.use(router.routes());
+  app.use(router.allowedMethods());
+
+  await app.listen({ port: 8000 });
+} catch (error) {
+  console.error(error);
+}
